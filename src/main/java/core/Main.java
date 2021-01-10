36a0f23ed575da16fcc11e041cc4c02616bfe797
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2021-01-10 20:39:29 +0200

    added Inject and Dao annotatios, added class Injector, added own exception

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,10 +1,11 @@
 package core;
 
 import core.controller.ConsoleHandler;
+import core.lib.Injector;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleHandler consoleHandler = new ConsoleHandler();
+        ConsoleHandler consoleHandler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Input your value and risk for your bet");
         consoleHandler.handle();
     }
--- src/main/java/core/dao/BetDaoImp.java
+++ src/main/java/core/dao/BetDaoImp.java
@@ -1,9 +1,11 @@
 package core.dao;
 
 import core.db.Storage;
+import core.lib.Dao;
 import core.model.Bet;
 import java.util.List;
 
+@Dao
 public class BetDaoImp implements BetDao {
     @Override
     public void add(Bet bet) {
